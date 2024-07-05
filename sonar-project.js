009ed7eefddef42ae8970a8ba55c904f45f22bc6
5G-Devops <160012435+5G-Devops@users.noreply.github.com>
2024-07-05 20:39:01 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,14 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.40.109.48:9000/',
+  serverUrl: 'http://13.50.231.208:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_4f95478fe616d8f91577c00fb18deed499888d60',
-	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.login': 'EPJLb+P/oZ7HjWJr5Pu1NQomwoTc1jlOl8h5sJOZIPk=',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'Dev0ps',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
